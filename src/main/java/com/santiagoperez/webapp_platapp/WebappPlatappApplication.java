6a2e6ed469fca70664263e879d99edd441f44a29
Santiago <programmer.santiago@gmail.com>
2024-12-26 17:09:57 +0000

    mi primer commit

---
+++ src/main/java/com/santiagoperez/webapp_platapp/WebappPlatappApplication.java
@@ -0,0 +1,13 @@
+package com.santiagoperez.webapp_platapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappPlatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappPlatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiagoperez/webapp_platapp/WebappPlatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagoperez.webapp_platapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappPlatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
